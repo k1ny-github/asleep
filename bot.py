7a150433a712b623367def1ae1116194e5fe0fc1
k1ny-github <marknnanzi@gmail.com>
2024-12-12 17:58:10 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+API_TOKEN = 7581408945:AAFSgjWGxW8RIXFhsx6pkwY6X_xAyLq27Ws
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я бот, созданный с использованием aiogram.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
